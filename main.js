afffaeb5409d88155026b9c1755d80c72e9e6bcc
GwendolynHommes <SF@hommes.co.nl>
2021-05-23 09:37:03 +0200

    GH

---
+++ main.js
@@ -0,0 +1,65 @@
+let getal1;
+console.log ("getal1 = " + getal1);
+// in console zie ik dat getal1 niet gedefinieerd is
+
+//const getal2;
+//console.log ("getal2 = " + getal2);
+// de console geeft aan dat er een syntax fout is gemaakt.
+//Dit komt omdat de constante geen waard heeft
+//ik heb deze code even als opmering gezet om zo de code door te laten lopen.
+
+let getal3;
+getal3 = 3;
+console.log ("getal3 = " + getal3);
+// ikk rijg nu getal3 = 3 te zien in de console.
+
+getal3 = 5;
+console.log ("getal3 = " + getal3);
+// door de waarde aan te passen zie ik in de console ok de waarde veranderd
+
+let datatype;
+datatype = true;
+console.log (typeof datatype);
+//true en false zijn beide boolean. 
+
+let datatype1;
+datatype1 = 'Hello World';
+console.log (typeof  datatype1);
+//dit is een string, te herkennen aan de '-tekens.
+
+let datatype2;
+datatype2 = 17.5;
+console.log (typeof  datatype2);
+//dit is een number, te herkennen aan het getal, dit mag met of zonder punt.
+
+let datatype3;
+datatyp3 = 1n;
+console.log (typeof datatype3);
+//dit is een big integer, te herkennen aan de 'n' op het einde.
+
+let getala = 6;
+let getalb = 8;
+
+console.log (getala + getalb);
+console.log (getalb - getala);
+console.log (getala * getalb);
+console.log (getalb / getala);
+
+let d = 3;
+let v = 5;
+let t = 10;
+let tw = 2;
+
+console.log (d+v*t/tw);
+//antwoord is 28
+
+let number = 3;
+number--;
+console.log(number);
+//er wordt 1 opgeteld of afgetrokken van de gegeven waarde.
+//antwoorden zijn resp. 4 en 2
+
+//ik begin het te snappen en stop nu met javascript om verder te gaan met Salesforce.
+//het lijkt erg op c programmeren en andere talen. dus hier ga ik wel uitkomen.
+
+
